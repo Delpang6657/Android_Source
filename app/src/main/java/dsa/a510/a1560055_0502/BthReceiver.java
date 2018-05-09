564241dafaf4acf472fc5b3f9d474ad1e2c636f5
Delpang6657 <wnsduf4802@naver.com>
2018-05-09 09:57:31 +0900

    [2018-05-02] SensorAnd #12

---
+++ app/src/main/java/dsa/a510/a1560055_0502/BthReceiver.java
@@ -0,0 +1,35 @@
+package dsa.a510.a1560055_0502;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BthReceiver extends BroadcastReceiver {
+    public String sBthName, sAddress;
+
+    public BthReceiver(String sBthName) {
+        this.sBthName = sBthName;
+        this.sAddress = "";
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+//        throw new UnsupportedOperationException("Not yet implemented");
+        if (intent.getAction().equals(BluetoothDevice.ACTION_FOUND)) {
+            BluetoothDevice device;
+            device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+//            Toast.makeText(context, sName + "is found.", Toast.LENGTH_SHORT).show();
+            if (device.getName().equalsIgnoreCase(sBthName)) {
+                sAddress = device.getAddress();
+                Toast.makeText(context, sBthName + "is found: " + sAddress, Toast.LENGTH_SHORT).show();
+            }
+        } else if (intent.getAction().equals(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)) {
+            Toast.makeText(context, "Discovery is finished.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
